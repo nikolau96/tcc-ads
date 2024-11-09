4b6331ffbfdd54303164dd231510c524ef99a7b7
Nickolas Markus <113360684+nikolau96@users.noreply.github.com>
2024-11-09 18:09:20 -0300

    Estilização da tela de cadastro

--- cadastro/tela_cadastro_principal.php
+++ cadastro/tela_cadastro_principal.php
@@ -2,28 +2,45 @@
     <head>
         <title>Sistema de Gerenciamento de Clientes</title>
         <link rel="icon" type="image/png" sizes="16x16" href="../favicon.png">
+        <link rel="stylesheet" type="text/css" href="../css/style-cadastro.css">
     </head>
     <body>
-        <header>
-            <img src="../favicon.png" alt="Logo do Sistema">
+        <header class="cabecalho">
+            <img src="../logo/SGC_logo_nova.png" alt="Logo do Sistema" class="imgsys">
             <div id="linha"></div>
         </header>
         <div class="title">
-            <h3>Cadastro</h3>
+            <h2>CADASTRO</h2>
         </div>
-        <div>
+        <div class="tela-body">
             <div>
                 <label class="subtitle">O que deseja cadastrar?</label>
             </div>
-            <div>
-                <a href="../cadastro/tela_cadastro_atendimento.php">Atendimento</a>
-                <a href="../cadastro/tela_cadastro_cliente.php">Cliente</a>
-                <a href="../cadastro/tela_cadastro_vendedor.php">Vendedor</a>
-                <a href="../cadastro/tela_cadastro_user.php">Usu&aacute;rio</a>
+            <div class="container">
+                <span class="container-block" id="container-atendimento">
+                    <img src="../assets/atendee_icon.png"> 
+                    <br><br><br>   
+                    <a href="../cadastro/tela_cadastro_atendimento.php">Atendimento</a>
+                </span>
+                <span class="container-block" id="container-cliente">
+                    <img src="../assets/customer_icon.png">
+                    <br><br><br>
+                    <a href="../cadastro/tela_cadastro_cliente.php">Cliente</a>
+                </span>
+                <span class="container-block" id="container-vendedor">
+                    <img src="../assets/seller_icon.png">
+                    <br><br><br>
+                    <a href="../cadastro/tela_cadastro_vendedor.php">Vendedor</a>
+                </span>
+                <span class="container-block" id="container-user">
+                    <img src="../assets/user_icon.png">
+                    <br><br><br>
+                    <a href="../cadastro/tela_cadastro_user.php">Usu&aacute;rio</a>
+                </span>
             </div>
         </div>
         <div>
-            <button type="button"><a href="../home/tela_home.php">Voltar</a></button>
+            <button type="button" id="btn-voltar"><a href="../home/tela_home.php">Voltar</a></button>
         </div>
     </body>
 </html>
